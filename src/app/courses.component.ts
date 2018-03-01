fd1466202b99586c497844499806914c62439ae3
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-01 18:06:12 +0000

    master Unit 4 - Displaying Data and Handling Events - Adding Bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,13 +4,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ title }}</h2>
-        <img src="{{ imageUrl }}"/>
-        <img [src]="imageUrl"/>
+            <button class="btn btn-primary">Save</button>
         `
 })
 
 export class CoursesComponent{
-    title = "List of courses";
-    imageUrl = "https://thumbs.dreamstime.com/b/construction-modern-buildin-2730290.jpg";
 }
