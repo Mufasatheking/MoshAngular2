75a7857fdf0437dfc6fbcb0b7dfbb09613298eeb
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-01 18:55:57 +0000

    master Unit 4 - Displaying Data and Handling Events - Custom Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SummaryPipe } from './summary.pipe';
 import { AuthorsService } from './authors.service';
 import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -16,7 +17,8 @@ import { AuthorsComponent } from './authors/authors.component';
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
