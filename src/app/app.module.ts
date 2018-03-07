46e47e6de6a85b7861e7a3aeed1a1d81886a7a03
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-07 18:32:42 +0000

    deleted unneeded code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,19 @@
-import { SummaryPipe } from './summary.pipe';
 import { AuthorsService } from './authors.service';
 import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { CoursesComponent } from "./courses.component";
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
-import { TitleCaseComponent } from './title-case/title-case.component';
-import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent,
-    SummaryPipe,
-    TitleCaseComponent,
-    TitleCasePipe
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
