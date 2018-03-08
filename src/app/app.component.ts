750b2e204325b289060f24ac631bb381d34466f0
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-08 13:28:47 +0000

    Unit6 - Directives - ngFor and Trackby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
+  courses 
+
+  loadCourses(){
+    this.courses = [
     {id:1, name: 'course1'},
     {id:2, name: 'course2'},
     {id:3, name: 'course3'},
   ];
+}
 
-  onAdd(){
-    this.courses.push({id:4, name: 'course4'});
-  }
-
-  onChange(course){
-    course.name = 'Updated';
+  trackCourse(index, course){
+    course ? course.id : undefined;
   }
 }
