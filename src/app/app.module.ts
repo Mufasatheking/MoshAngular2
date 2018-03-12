4e38c0729def87f48679ce85f31b87f6f81daa0e
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-12 17:34:09 +0000

    Unit6 - Clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,13 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from "./courses.component";
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
-import { InputFormatDirective } from './input-format.directive';
-import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent,
-    InputFormatDirective,
-    ZippyComponent
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
