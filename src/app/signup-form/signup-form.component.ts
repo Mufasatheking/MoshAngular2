a173e32e92777b3ad646761f88c86b7cc9a91976
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-13 18:46:44 +0000

    Unit8 - Reactive Forms - Nested FormGroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -10,19 +10,13 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('',
-      Validators.required,
-      UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   });
 
-  login(){
-    this.form.setErrors({
-      invalidLogin:true
-    });
-  }
-
   get username(){
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
