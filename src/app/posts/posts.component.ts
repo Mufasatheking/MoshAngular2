fb6e00b6bff02dae3d409591916d4740e199ea63
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-19 18:08:45 +0000

    Unit9 - Consuming HTTP Services - Handling Expected Errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -14,44 +14,66 @@ export class PostsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.service.getPosts().subscribe(response => {
-      this.posts = response.json();
-    }, error => {
-      alert('An unexpcted error occured');
-      console.log(error);
-    })
+      this.service.getPosts()
+        .subscribe(
+          response => {
+            this.posts = response.json();
+          },
+          error => {
+            alert('An unexpcted error occured');
+            console.log(error);
+          })
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
 
-    this.service.createPost(post).subscribe(response => {
-      post['id'] = response.json().id;
-      this.posts.splice(0, 0, post);
-      console.log(response.json());
-    }, error => {
-      alert('An unexpcted error occured');
-      console.log(error);
-    })
+    this.service.createPost(post)
+      .subscribe(
+        response => {
+          post['id'] = response.json().id;
+          this.posts.splice(0, 0, post);
+          console.log(response.json());
+        },
+        (error: Response) => {
+          if(error.status === 400) {
+            // this.form.setErrors(error.json());
+          }
+          else {
+            alert('An unexpcted error occured');
+            console.log(error);
+          }
+        });
   }
 
   updatePost(post){
-    this.service.updatePost(post).subscribe(response => {
-      console.log(response.json());
-    }, error => {
-      alert('An unexpcted error occured');
-      console.log(error);
-    })
+    this.service.updatePost(post)
+      .subscribe(
+        response => {
+          console.log(response.json());
+        },
+        error => {
+          alert('An unexpcted error occured');
+          console.log(error);
+      })
   }
 
   deletePost(post){
-    this.service.deletePost(post.id).subscribe(response => {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-    }, error => {
-      alert('An unexpcted error occured');
-      console.log(error);
-    })
+    this.service.deletePost(post.id)
+      .subscribe(
+        response => {
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+        }, 
+        (error: Response) => {
+          if(error.status === 404) {
+            alert('This post has already been deleted');
+          }
+          else {
+            alert('An unexpcted error occured');
+            console.log(error);
+          }
+        });
   }
 }
