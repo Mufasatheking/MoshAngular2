da8a62a7eb07c35cb63abaf0bcbec2e5eb6af77e
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-19 13:37:48 +0000

    Unit9 - Consuming HTTP Services - Extracting a Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PostService } from './services/post.service';
 import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -24,6 +25,7 @@ import { PostsComponent } from './posts/posts.component';
     HttpModule
   ],
   providers: [
+    PostService,
     AuthorsService
   ],
   bootstrap: [AppComponent]
