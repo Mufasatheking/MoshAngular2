bd411906061a4601e739af5e902406646fbd1480
Mustafa Aleem <Mustafa.Aleem@rbi.co.uk>
2018-03-20 18:01:28 +0000

    Unit9 - Consuming HTTP Services - Optimistic vs Pessimistic Updates

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -23,15 +23,16 @@ export class PostsComponent implements OnInit {
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
+    this.posts.splice(0, 0, post);
     input.value = '';
 
     this.service.create(post)
       .subscribe(
         newPost => {
           post['id'] = newPost.id;
-          this.posts.splice(0, 0, post);
         },
         (error: AppError) => {
+          this.posts.splice(0,1);
           if(error instanceof BadInputError) {
             // this.form.setErrors(error.originalError);
           }
@@ -50,13 +51,14 @@ export class PostsComponent implements OnInit {
   }
 
   deletePost(post){
-    this.service.delete(333)
+    let index = this.posts.indexOf(post);
+    this.posts.splice(index, 1);
+
+    this.service.delete(post.id)
       .subscribe(
-        () => {
-          let index = this.posts.indexOf(post);
-          this.posts.splice(index, 1);
-        },
+        null,
         (error: AppError) => {
+          this.posts.splice(index, 0, post);
           if(error instanceof NotFoundError) {
             alert('This post has already been deleted');
           }
